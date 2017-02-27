0c8c2e9229166ad1ef7fb7265ebfcd883b79c8a5
JonBrune <jonik12@ru.is>
2017-02-27 22:14:02 +0000

    cleaning

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace WebStore.Controllers
             return RedirectToAction("Index", "Products");
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
